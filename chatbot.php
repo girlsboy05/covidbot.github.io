68f1f884a475cee1a750b9fb6f78112e23446b4b
Saif Abrar <girlsboy05@gmail.com>
2020-04-19 17:34:27 +0600

    Update chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -2,7 +2,7 @@
 
     if(isset($_REQUEST['hub_challenge'])) {
         $challenge = $_REQUEST['hub_challenge'];
-        $token = $_REQUEST[]'hub_verify_token';
+        $token = $_REQUEST['hub_verify_token'];
     }
     if($token=="covidbot") {
         echo $challenge;
